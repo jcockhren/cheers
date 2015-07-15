5ec67afabf82f7968497e036f5c53fa398e67b0a
Hannah Smithmier <smithmier.hannah@gmail.com>
2015-07-15 08:41:08 -0500

    project complete

---
+++ csharp/Cheers/Cheers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace cheers
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+			Console.Write("What is your first name? ");
+			string firstName = Console.ReadLine();
+			string grandFinale = firstName + " is ... GRAND!";
+
+			for (int i = 0; i < firstName.Length; i++) {
+				string gimmeA = "Give me a ";
+				string gimmeAn = "Give me an ";
+
+
+				if(firstName[i] == char.Parse("a") || firstName[i] ==  char.Parse("e") || firstName[i] ==  char.Parse("i") || firstName[i] == char.Parse("o") || firstName[i] ==  char.Parse("u")) {
+					Console.WriteLine (gimmeAn + firstName[i]);
+
+
+				} else {
+					Console.WriteLine (gimmeA + firstName[i]);
+
+				}
+
+			}
+			Console.WriteLine (grandFinale);
+
+		}
+	}
+}
